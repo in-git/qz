0a9e4dcd88b00bcb43dbb954393ecb12ec3b6f11
in-git <444891953@qq.com>
2024-10-14 17:19:15 +0800

    添加Dist目录

---
+++ dist/assets/utils-CIuFtj-V.js
@@ -0,0 +1,1 @@
+const o=n=>{const e=document.createElement("a");e.href=n,e.target="_blank",document.body.append(e),e.click(),setTimeout(()=>{var t;return(t=e.parentNode)==null?void 0:t.removeChild(e)})};export{o};
